999a10cc8a644a0cb2f77d3e816b5dd94c236173
talhahacker123 <59144474+talhahacker123@users.noreply.github.com>
2021-09-23 03:52:17 +1200

    Update framework.py

--- Hackeroof/framework.py
+++ Hackeroof/framework.py
@@ -37,50 +37,6 @@ def device():
 # out = p.stdout.read()
      st.success(k.decode('UTF-8'))
     
-
-     
-
- elif check_name=='nt' and check_platform=="win32":
-
-   st.write('This is Windows OS')
-
-    
-    
-    
-   st.title('Device Detector')
-   st.subheader('It displays devices connected to windows machine')
-
-   p = subprocess.Popen("powershell.exe  Get-PnpDevice -PresentOnly -class WPD, DiskDrive, HIDCLASS -status ok | Where-Object { $_.InstanceId -match '^USB' }",stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-   p.communicate()
-   o,e=p.communicate()
-   st.success(o.decode('ascii'))
-   os.system("cls")
-   drive_types = {
-                
-                #  win32file.DRIVE_UNKNOWN : "Unknown\nDrive type can't be determined.",
-                 win32file.DRIVE_REMOVABLE : "Removable\nDrive has removable media. This includes all floppy drives and many other varieties of storage devices.",
-                 win32file.DRIVE_FIXED : "Fixed\nDrive has fixed (nonremovable) media. This includes all hard drives, including hard drives that are removable.",
-                 win32file.DRIVE_REMOTE : "Remote\nNetwork drives. This includes drives shared anywhere on a network.",
-                 win32file.DRIVE_CDROM : "CDROM\nDrive is a CD-ROM. No distinction is made between read-only and read/write CD-ROM drives.",
-                 win32file.DRIVE_RAMDISK : "RAMDisk\nDrive is a block of random access memory (RAM) on the local computer that behaves like a disk drive.",
-                 win32file.DRIVE_NO_ROOT_DIR : "The root directory does not exist." 
-              }
-
-   drives = win32api.GetLogicalDriveStrings().split('\x00')[:-1]
-
-   for device in drives:
-     type = win32file.GetDriveType(device)
-    
-     st.write("Drive: %s" % device)
-     st.write(drive_types[type])
-     st.write("-"*72)
-     st.title('Want to see recently connected "USB Devices" on computer..... ')
-    if st.button('Click here'):
-     p = subprocess.Popen("powershell.exe   Get-ItemProperty -Path 'HKLM:\\SYSTEM\\CurrentControlSet\\Enum\\USBSTOR\\*\\*' | Select FriendlyName",stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-     p.communicate()
-     o,e=p.communicate()
-     st.write('output: '+o.decode('ascii'))
-
 def subscanner():
  check_name=os.name
  if check_name=='nt':
